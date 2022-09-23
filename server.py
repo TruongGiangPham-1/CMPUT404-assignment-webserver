b1cac5852f482e2858a569efef083876642d93b3
truong <truonggi@ualberta.ca>
2022-09-23 11:00:20 -0600

    now works on albitrary request folder

--- server.py
+++ server.py
@@ -2,7 +2,6 @@
 from logging.config import listen
 import socketserver
 import os
-
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -56,13 +55,31 @@ class MyWebServer(socketserver.BaseRequestHandler):
                 return
 
             
-            # Handle valid html file request like GET / or GET /deep/
+            # Handle valid html file request like GET / or GET /deep/  
+            # send index.html
 
             #print(dataStrList)
             #print("we got ", getPath(dataStrList[0]))
             print ("Got a request of: %s\n" % self.data)
             isFavicon = True
             localPath = getPath(dataStrList[0])  # this is / or /deep or /deep/
+
+            ret = checkGETPath(localPath, dataStrList[0])
+            if (ret != "OK dir path" and ret != "OK file path"):
+                # then ret == one of 301 or 404 headers
+                self.request.sendall(bytearray(ret, 'utf-8'))
+                return
+            elif (ret == "OK dir path"):
+                # return index.html
+                indexHtmlHeader =  returnIndexHtml(localPath)
+                self.request.sendall(bytearray(indexHtmlHeader, 'utf-8'))
+                return
+            elif (ret == "OK file path"):
+                # here, localpath = "/.../___.html"
+                return
+
+
+            # if its OK dir path, we just return index html
             absPath = getFullPath(localPath)
             #print("abs path is ", absPath)
 
@@ -78,7 +95,11 @@ class MyWebServer(socketserver.BaseRequestHandler):
             msgContent = ""
 
             #TODO: instead of hardcoding index.html, we find whatever html file in that directory
-            #  
+            #  TODO: DO NOT HARDCODE INDEX.HTML AND HE MIGHT MAKE ANOTHER   
+            htmlFileName = localPath.replace('/', '') + ".html"
+            if (localPath == '/'):
+                htmlFileName = "index.html"  
+            #TODO: ANOTHER THING WE NEED TO do is find any html file in that directory
             with open(absPath + "index.html") as file:
                 msgContent = file.read()
                 #print(msgContent)
@@ -97,6 +118,92 @@ class MyWebServer(socketserver.BaseRequestHandler):
         # use sendall for headers + file content
         #self.request.sendall(bytearray("OK",'utf-8'))
         # write("")
+
+def checkGETPath(localPath, requestMsg):
+    # TODO: check if the localPath file exist in the directory
+    # Loop invariant: local path = /.....
+    # 1. check if the /deep is a valid directory
+    # 2. if not then check if this is a valid path to directory
+
+    if ("GET" not in requestMsg):  # TODO: this should be at the very top
+        return return405Header()
+    print("in checkGET localpath is ", localPath)
+    validPaths = {"/", "/deep/"}
+    if (localPath[-1] != '/' and ".html" not in localPath):  # case: folder that  doesnt end with / 
+        print()
+        print("301 error")
+        print()  
+        return return301Header()
+    else:  # how to do it 
+        # here local path is /../.html(check if html is valid) or /folder/(check if folder is valid) 
+        if (".html" in localPath):  # check if this path to html file exist
+            flag = os.path.exists(getFullPath(localPath)) 
+            print()
+            print("requestedPath exist? ", flag)
+            print()
+            if (flag):
+                return "OK file path"
+            return return404Header()  # case: path to .html file dont exist
+        else:  # case requested path is /../folder/ to a valid folder format
+            print()
+            flag = os.path.exists(getFullPath(localPath))
+            if (flag):  # this folder exist
+                return "OK dir path"
+            return return404Header()  # folder dont exist
+    # else this path is fine
+    return "OK path"
+
+def returnIndexHtml(localPath: str):
+    status_code = 200
+    reason_phrase = "OK"
+    Status_Line = f'HTTP/1.1 {str(status_code)} {reason_phrase}\r\n'
+    content_type = f'Content-Type: text/html; charset=UTF-8\r\n'
+
+    absPath = getFullPath(localPath)  # abs path neesd to be /.../folder/index.html
+    with open(absPath + "index.html") as file:
+        msgContent = file.read()
+        #print(msgContent)
+
+        l = len(msgContent.encode('utf-8'))
+        msgLength = f'Content-Length: {str(l)}\r\n'
+        ResponseHeader = f"{Status_Line}{content_type}{msgLength}\r\n{msgContent}"
+        #print(ResponseHeader)
+        # no I will get the file
+        # append htmlcontent to the header
+
+        return ResponseHeader 
+
+def return301Header():
+    #HTTP/1.1 301 Moved Permanently
+    #Location: http://www.example.org/index.asp
+    location = "http://127.0.0.1:8080/deep/"
+    status_code = 301
+    reason_phrase = "Moved Permanently"
+    Status_Line = f'HTTP/1.1 {str(status_code)} {reason_phrase}\r\n'
+    ResponseHeader = f"{Status_Line}Location: {location}\r\n"
+    return ResponseHeader
+
+def return404Header():
+    status_code = 404
+    reason_phrase = "Not Found"
+    Status_Line = f'HTTP/1.1 {str(status_code)} {reason_phrase}\r\n'
+    msg = "404 Not Found"
+    l = len(msg.encode('utf-8'))
+    msgLength = f'Content-Length: {str(l)}\r\n'
+    ResponseHeader = f"{Status_Line}{msgLength}\r\n{msg}"
+    print(ResponseHeader)
+    return ResponseHeader
+
+def return405Header():
+    status_code = 405
+    reason_phrase = "Method Not Allowed"
+    Status_Line = f'HTTP/1.1 {str(status_code)} {reason_phrase}\r\n'
+    msg = "405 Method Not Allowed"
+    l = len(msg.encode('utf-8'))
+    msgLength = f'Content-Length: {str(l)}\r\n'
+    ResponseHeader = f"{Status_Line}{msgLength}\r\n{msg}"
+    print(ResponseHeader)
+    return ResponseHeader
 """
 given absolute path to css filename   : ...../base.css for example
 
